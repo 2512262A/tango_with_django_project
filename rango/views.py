9f6f28499f726f7a4b75d4646d6a45de99248c98
Muhammad Amrullah <2512262A@student.gla.ac.uk>
2021-02-11 14:51:05 +0000

    TWD Chapter 10 (finished).

--- rango/views.py
+++ rango/views.py
@@ -26,7 +26,6 @@ def index(request):
     context_dict['pages'] = page_list
 
     visitor_cookie_handler(request)
-    context_dict['visits'] = request.session['visits']
 
     response = render(request, 'rango/index.html', context=context_dict)
     return response
@@ -34,7 +33,11 @@ def index(request):
 def about(request):
     context_dict = {'yourname': 'Muhammad Rafli Amrullah'}
 
-    return render(request, 'rango/about.html', context=context_dict)
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'rango/about.html', context=context_dict)
+    return response
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
